e2748564ab4772753ffb586044686ca64408545b
muhammad-usman-108 <umuhammad202@yahoo.com>
2022-09-07 10:10:43 +0200

    Kotlin: Declarations

---
+++ Declarations/src/main/kotlin/Declarations.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    val number1 = 25
+    var number2 = 10;
+    number2 = 20;
+    if (number1>number2) {
+        println("Num1 is greater than Num2")
+    }
+    else {
+        println("Num2 is greater than Num1")
+    }
+}
